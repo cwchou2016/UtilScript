0b61cb9d23de439cb285df4584276723284c367f
cwchou2016 <s955401@gmail.com>
2020-02-24 11:02:07 +0800

    complete get_files_link() add docstring

--- Utils/KMS/Document.py
+++ Utils/KMS/Document.py
@@ -1,8 +1,13 @@
 class Document:
     def __init__(self, doc_id, soup):
+        """
+        :param doc_id:
+        :param soup: BeautifulSoup of document page.
+        """
         self._doc_id = doc_id
         self._soup = soup
         self.files = {}
+        self.load_files()
 
     def load_files(self):
         """
@@ -20,6 +25,9 @@ class Document:
             self.files[f_name] = link
 
     def get_latest_version(self):
+        """
+        :return: The latest version number from the document page
+        """
         ver = self._soup.find("span", {"id": "ctl00_cp_latestVersion"})
 
         if ver is None:
@@ -27,5 +35,16 @@ class Document:
 
         return ver
 
-    def download_files(self):
+    def get_files_link(self):
+        """
+        Get all download links of files if download is available.
+        :return: dictionary of files with its download links.
+        """
+        return self.files
+
+    def get_view_link(self):
+        """
+        Generate the links of the preview window.
+        :return: dictionary of files with its view links.
+        """
         pass
