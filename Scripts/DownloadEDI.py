f40023e156243cebbf975f3dca3fc0e5c174624e
cwchou2016 <s955401@gmail.com>
2020-04-04 09:08:41 +0800

    aquire download path from environment variable

--- Scripts/DownloadEDI.py
+++ Scripts/DownloadEDI.py
@@ -1,8 +1,11 @@
 import getpass
+import os
 
 from Utils.BOS.BloodServer import BloodServer
 
 edi = []
+
+path = os.environ.get('DOWNLOAD_PATH')
 
 with open("EDI.txt", "r") as f:
     for line in f.readlines():
@@ -20,7 +23,7 @@ for i in edi:
 
     r = bs.confirm_order(i)
     print(r.text)
-    bs.download_edi(i)
+    bs.download_edi(i, path)
 
 r = bs.logout()
 print(r.text)
