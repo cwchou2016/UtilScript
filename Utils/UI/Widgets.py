32a6188c7f2513b076032ee1f370e173ef23c215
cwchou2016 <s955401@gmail.com>
2020-04-04 12:03:59 +0800

    create on_btn_dir_clicked()

--- Utils/UI/Widgets.py
+++ Utils/UI/Widgets.py
@@ -1,4 +1,5 @@
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import QApplication, QWidget, QFileDialog
 from PyQt5.uic import loadUi
 
 
@@ -7,14 +8,21 @@ class EdiDownloadWidget(QWidget):
         super(EdiDownloadWidget, self).__init__(parent)
         loadUi("EdiDownloadWidget.ui", self)
 
+        self._path = ""
+
+    @QtCore.pyqtSlot()
+    def on_btn_dir_clicked(self):
+        self._path = QFileDialog.getExistingDirectory()
+        self.update()
+
+    def update(self):
+        self.line_path.setText(self._path)
 
 
 if __name__ == "__main__":
     import sys
 
     app = QApplication(sys.argv)
-
     window = EdiDownloadWidget()
     window.show()
-
     sys.exit(app.exec_())
