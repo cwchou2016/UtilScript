4c3a0572cddcce58fae86653eaaf2ee2580cd88b
cwchou2016 <s955401@gmail.com>
2020-04-06 21:02:26 +0800

    implement QThreadPool and for validation concept

--- Utils/UI/Widgets.py
+++ Utils/UI/Widgets.py
@@ -1,6 +1,37 @@
-from PyQt5.QtCore import pyqtSlot
+import sys
+import time
+import traceback
+
+from PyQt5.QtCore import pyqtSlot, QRunnable, QThreadPool, QObject, pyqtSignal
 from PyQt5.QtWidgets import QApplication, QWidget, QFileDialog, QTableWidgetItem, QAbstractItemView
 from PyQt5.uic import loadUi
+
+sys._excepthook = sys.excepthook
+
+
+def exception_hook(exctype, value, traceback):
+    sys._excepthook(exctype, value, traceback)
+    sys.exit(1)
+
+
+sys.excepthook = exception_hook
+
+
+class Signals(QObject):
+    validation = pyqtSignal(int, str)
+
+
+class ValidateOrder(QRunnable):
+
+    def __init__(self, row: int, edi: str, parent=None):
+        super(ValidateOrder, self).__init__()
+        self.signal = Signals()
+        self.edi = edi
+        self.row = row
+
+    def run(self):
+        time.sleep(5)
+        self.signal.validation.emit(self.row, "OK")
 
 
 class EdiDownloadWidget(QWidget):
@@ -14,6 +45,10 @@ class EdiDownloadWidget(QWidget):
         self.table_edi.setSelectionMode(QAbstractItemView.NoSelection)
         self.table_edi.setEditTriggers(QAbstractItemView.NoEditTriggers)
 
+        self.btn_download.setEnabled(False)
+
+        self.pool = QThreadPool()
+
     @pyqtSlot()
     def on_label_clicked(self):
         row = self.table_edi.currentRow()
@@ -21,18 +56,33 @@ class EdiDownloadWidget(QWidget):
 
     @pyqtSlot()
     def on_btn_add_clicked(self):
+        self.btn_download.setEnabled(False)
+
         edi = self.line_order.text()
 
         if edi != "":
             row = self.table_edi.rowCount()
             self.table_edi.insertRow(row)
-            self.table_edi.setItem(row, 0, QTableWidgetItem(edi))
             self.table_edi.setVerticalHeaderItem(row, QTableWidgetItem("刪"))
+
+            self.table_edi.setItem(row, 0, QTableWidgetItem(edi))
+
+            work = ValidateOrder(row, str(edi))
+            work.signal.validation.connect(self.update_check)
+            self.pool.start(work)
+
             self.line_order.setText("")
 
         self.line_order.setFocus()
 
-        self.update()
+    def update_check(self, row, string):
+        self.table_edi.setItem(row, 1, QTableWidgetItem(string))
+        row_count = self.table_edi.rowCount()
+        for idx in range(row_count):
+            if self.table_edi.item(idx, 1) is None:
+                return
+
+        self.btn_download.setEnabled(True)
 
     @pyqtSlot()
     def on_btn_dir_clicked(self):
@@ -47,6 +97,10 @@ if __name__ == "__main__":
     import sys
 
     app = QApplication(sys.argv)
-    window = EdiDownloadWidget()
-    window.show()
+    try:
+        window = EdiDownloadWidget()
+        window.show()
+    except Exception as e:
+        print(e)
+        traceback.print_exc()
     sys.exit(app.exec_())
