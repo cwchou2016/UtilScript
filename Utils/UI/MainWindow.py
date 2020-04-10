c70a47f651aeec43f994688adcad8b0a41c9d184
cwchou2016 <s955401@gmail.com>
2020-04-11 00:56:44 +0800

    complete loading orders to the table

--- Utils/UI/MainWindow.py
+++ Utils/UI/MainWindow.py
@@ -1,8 +1,9 @@
 import sys
+import time
 import traceback
 
-from PyQt5.QtCore import QObject, pyqtSlot
-from PyQt5.QtWidgets import QApplication, QDialog, QMainWindow, QFileDialog
+from PyQt5.QtCore import QObject, pyqtSlot, QThread, pyqtSignal
+from PyQt5.QtWidgets import QApplication, QDialog, QMainWindow, QFileDialog, QTableWidgetItem
 
 from Utils.UI.Widgets import LoginWidget, EdiDownloadWidget
 
@@ -21,6 +22,19 @@ class Signals(QObject):
     pass
 
 
+class VerifyEDI(QThread):
+    verified = pyqtSignal(int, str)
+
+    def __init__(self, row: int, edi: str):
+        super(VerifyEDI, self).__init__()
+        self._edi = edi
+        self._row = row
+
+    def run(self) -> None:
+        time.sleep(3)
+        self.verified.emit(self._row, str("OK"))
+
+
 class MainWindow(EdiDownloadWidget, QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
@@ -29,6 +43,8 @@ class MainWindow(EdiDownloadWidget, QMainWindow):
 
         self.line_path.setText("C:\\Blood")
 
+        self.table_edi.verticalHeader().sectionClicked.connect(self.on_v_label_clicked)
+
     @pyqtSlot()
     def on_btn_dir_clicked(self):
         path = QFileDialog().getExistingDirectory()
@@ -36,11 +52,36 @@ class MainWindow(EdiDownloadWidget, QMainWindow):
 
     @pyqtSlot()
     def on_btn_add_clicked(self):
-        pass
+        edi = self.line_order.text()
+
+        if edi == "":
+            self.line_order.setFocus()
+            return
+
+        row = self.table_edi.rowCount()
+        self.table_edi.insertRow(row)
+        self.table_edi.setVerticalHeaderItem(row, QTableWidgetItem("刪"))
+
+        edi = self.line_order.text()
+        self.table_edi.setItem(row, 0, QTableWidgetItem(edi))
+        self.verify_thread = VerifyEDI(row, edi)
+        self.verify_thread.verified.connect(self.update_check)
+        self.verify_thread.start()
+
+        self.line_order.setText("")
+        self.line_order.setFocus()
 
     @pyqtSlot()
     def on_btn_download_clicked(self):
         pass
+
+    @pyqtSlot()
+    def on_v_label_clicked(self):
+        row = self.table_edi.currentRow()
+        self.table_edi.removeRow(row)
+
+    def update_check(self, row: int, msg: str):
+        self.table_edi.setItem(row, 1, QTableWidgetItem(msg))
 
 
 class LoginWindow(QDialog, LoginWidget):
